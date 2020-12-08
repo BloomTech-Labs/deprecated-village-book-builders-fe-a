d3165418bffc7fc98832016fcf0a946862492dee
Matthew Mulford <mhmulford0@gmail.com>
2020-12-08 12:39:41 -0600

    added Affix component to make nav sticky for mobile-first design

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Menu } from 'antd';
+import { Menu, Affix } from 'antd';
 import { HomeFilled, SettingOutlined } from '@ant-design/icons';
 const { SubMenu } = Menu;
 
@@ -11,20 +11,22 @@ const StyledNav = styled(Menu)`
 
 function Nav() {
   return (
-    <StyledNav mode="horizontal">
-      <Menu.Item key="mail" icon={<HomeFilled />}>
-        <Link to="/">Home</Link>
-      </Menu.Item>
-      <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Profiles">
-        <Menu.ItemGroup>
-          <Menu.Item key="village">Village Profile</Menu.Item>
-          <Menu.Item key="library">Library Profile</Menu.Item>
-          <Menu.Item key="school">School Profile</Menu.Item>
-          <Menu.Item key="students">Studen Profiles</Menu.Item>
-          <Menu.Item key="user">Your Profile</Menu.Item>
-        </Menu.ItemGroup>
-      </SubMenu>
-    </StyledNav>
+    <Affix offsetTop="0">
+      <StyledNav mode="horizontal">
+        <Menu.Item key="mail" icon={<HomeFilled />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Profiles">
+          <Menu.ItemGroup>
+            <Menu.Item key="village">Village Profile</Menu.Item>
+            <Menu.Item key="library">Library Profile</Menu.Item>
+            <Menu.Item key="school">School Profile</Menu.Item>
+            <Menu.Item key="students">Studen Profiles</Menu.Item>
+            <Menu.Item key="user">Your Profile</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </StyledNav>
+    </Affix>
   );
 }
 
